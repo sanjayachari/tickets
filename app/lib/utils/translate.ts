a6440d1af4783859eee7e71c21d1a823498094dd
sanjay achari <sanjayacharya992@gmail.com>
2025-09-05 16:32:31 +0530

    build err

--- app/lib/utils/translate.ts
+++ app/lib/utils/translate.ts
@@ -1,29 +1,29 @@
-import translator from 'open-google-translator';
+// import translator from 'open-google-translator';
 
-export type SupportedLang = 'en' | 'ru' | 'hi';
+// export type SupportedLang = 'en' | 'ru' | 'hi';
 
-export const translateText = async (
-  text: string | string[],
-  targetLang: SupportedLang = 'en'
-): Promise<string | string[]> => {
-  try {
-    if (targetLang === 'en') return text;
-    console.log('texttext', text);
-    console.log('textargetLangttext', targetLang);
-    const listToTranslate = Array.isArray(text) ? text : [text];
+// export const translateText = async (
+//   text: string | string[],
+//   targetLang: SupportedLang = 'en'
+// ): Promise<string | string[]> => {
+//   try {
+//     if (targetLang === 'en') return text;
+//     console.log('texttext', text);
+//     console.log('textargetLangttext', targetLang);
+//     const listToTranslate = Array.isArray(text) ? text : [text];
 
-    // @ts-ignore: library types are missing
-    const result: any[] = await translator.TranslateLanguageData({
-      listOfWordsToTranslate: listToTranslate,
-      fromLanguage: 'en',
-      toLanguage: targetLang
-    });
-    console.log('!!!!!' , result)
-    const translatedList = result.map(item => item.translatedText);
-    console.log('====' , Array.isArray(text) ? translatedList : translatedList[0])
-    return Array.isArray(text) ? translatedList : translatedList[0];
-  } catch (err) {
-    console.error('Translation error:', err);
-    return text;
-  }
-};
+//     // @ts-ignore: library types are missing
+//     const result: any[] = await translator.TranslateLanguageData({
+//       listOfWordsToTranslate: listToTranslate,
+//       fromLanguage: 'en',
+//       toLanguage: targetLang
+//     });
+//     console.log('!!!!!' , result)
+//     const translatedList = result.map(item => item.translatedText);
+//     console.log('====' , Array.isArray(text) ? translatedList : translatedList[0])
+//     return Array.isArray(text) ? translatedList : translatedList[0];
+//   } catch (err) {
+//     console.error('Translation error:', err);
+//     return text;
+//   }
+// };
