dcf146a435d660ec1472f937d0b83533777533fc
sanjay achari <sanjayacharya992@gmail.com>
2025-09-25 15:16:06 +0530

    update

--- app/lib/helper/apiClient.ts
+++ app/lib/helper/apiClient.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "https://core.staybook.in/api/v1"
+// const BASE_URL = "https://core.staybook.in/api/v1"
+const BASE_URL = "http://localhost:8080/api/v1"
 const apiClient = axios.create({
   baseURL: BASE_URL,
   headers: {
